71bb84e31e4610572b0f6b746ea7e85b33d2d29b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-30 09:41:59 +0800

    kata/valid_parentheses.cpp

--- tests/find_the_vowels.cpp
+++ tests/find_the_vowels.cpp
@@ -1,21 +1,14 @@
-#include <catch2/catch_test_macros.hpp>
 #include <vector>
 #include <string>
 
-// Declaration of the function under test (implemented in kata/find_the_vowels.cpp)
 std::vector<int> vowelIndices(const std::string& word);
+#include "cw_compat.hpp"
 
-TEST_CASE("vowelIndices simple tests") {
-    std::vector res{2, 4};
-    REQUIRE(vowelIndices("super") == res);
-}
-
-TEST_CASE("vowelIndices simple test1") {
-    std::vector res{1, 3, 6};
-    REQUIRE(vowelIndices("orAnge") == res);
-}
-
-TEST_CASE("vowelIndices simple test2") {
-    std::vector res{5, 9, 11, 12, 18, 19, 24, 25, 28, 34, 35, 36, 37, 39, 53, 63};
-    REQUIRE(vowelIndices("RNmfYmXSegaejlSXXiObWkRaafbeVXQlVOiUakOkQlkkXdScmTWhibWdhkNTkbacbfbjlbcQ") == res);
-}
+Describe(vowelIndicesTest)
+{
+    It(SimpleTests)
+    {
+        std::vector<int> res { 2, 4 };
+        Assert::That(vowelIndices("super"), Equals(res));
+    }
+};
