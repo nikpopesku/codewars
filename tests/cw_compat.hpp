0a6316292537f680746a96dc4ddaf4fddf7d42d6
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-30 09:47:59 +0800

    kata/valid_parentheses.cpp

--- tests/cw_compat.hpp
+++ tests/cw_compat.hpp
@@ -3,6 +3,10 @@
 #include <string>
 #include <catch2/catch_all.hpp>
 
+// Forward declarations for helpers referenced by generic test utilities
+// Declared in kata/valid_parentheses.cpp and tested in tests/valid_parentheses.cpp
+bool validParentheses(const std::string&);
+
 // Map Codewars BDD-style to Catch2 equivalents
 #define Describe(name) TEST_CASE(#name)
 #define It(name)       SECTION(#name)
@@ -11,13 +15,13 @@
 #define Equals(expected) expected
 #define ExtraMessage(msg) (msg)
 
-//namespace cw_compat {
-//    inline void dotest(const std::string& str, bool expected) {
-//        bool actual = validParentheses(str);
-//        INFO("Incorrect answer for input = \"" << str << "\"");
-//        REQUIRE(actual == expected);
-//    }
-//}
+namespace cw_compat {
+    inline void dotest(const std::string& str, bool expected) {
+        bool actual = validParentheses(str);
+        INFO("Incorrect answer for input = \"" << str << "\"");
+        REQUIRE(actual == expected);
+    }
+}
 
 // Provide Assert::That(...) API similar to Codewars
 namespace Assert {
