3ff2489ede8f52ea4dd77050deb8e4225ee48de5
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-30 15:25:05 +0800

    kata/mumbling.cpp

--- kata/mumbling.cpp
+++ kata/mumbling.cpp
@@ -1,9 +1,30 @@
 #include <string>
 
-class Accumul
-{
+using namespace std;
+
+class Accumul {
 public:
     static std::string accum(const std::string &s) {
-        return "asdf";
+        string response;
+        int counter = 1;
+
+        for (auto c: s) {
+            for (int i = 0; i < counter; ++i) {
+                if (i == 0) {
+                    response += toupper(c);
+                    continue;
+                }
+
+                response += tolower(c);
+            }
+
+            if (counter < static_cast<int>(s.size())) {
+                response += '-';
+            }
+
+            ++counter;
+        }
+
+        return response;
     }
 };
