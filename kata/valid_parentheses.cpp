493d5c9c9470b537ebe0e49463f8477359776b1d
npopesku <nikolay.popesku@unbiased.co.uk>
2025-10-30 09:55:45 +0800

    kata/valid_parentheses.cpp

--- kata/valid_parentheses.cpp
+++ kata/valid_parentheses.cpp
@@ -1,5 +1,20 @@
 #include <string>
+#include <stack>
+
+using namespace std;
 
 bool validParentheses(const std::string& str) {
-    return false;
+    std::stack s;
+
+    for (auto &c: str) {
+        if (c == '(') {
+            s.push_back(c);
+        } else {
+            if (s.empty()) {
+                return false;
+            }
+
+            s.pop();
+        }
+    }
 }
