892287ffb5572cd4ab1d789229a6c30217e92908
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-01 15:28:07 +0800

    refactoring

--- tests/square_every_digit.cpp
+++ tests/square_every_digit.cpp
@@ -1,6 +1,33 @@
-// Declaration to use the function implemented in quarter_of_the_year.cpp
-int square_digits(int num);
 #include "cw_compat.hpp"
+#include <string>
+#include <stack>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+int square_digits(int num) {
+    // Handle zero explicitly to avoid converting an empty string with stoi
+    if (num == 0) return 0;
+
+    stack<string> s;
+    string response;
+
+    while (num > 0) {
+        const int digit = num % 10;
+        num /= 10;
+
+        s.push(to_string(digit * digit));
+    }
+
+    while (!s.empty()) {
+        response += s.top();
+        s.pop();
+    }
+
+    return stoi(response);
+}
+
 
 Describe(Square_Every_Digit)
 {
