80fd54e8dc775bf334c273a6ddb222c816fef36a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-01 15:19:05 +0800

    refactoring

--- tests/find_the_vowels.cpp
+++ tests/find_the_vowels.cpp
@@ -1,14 +1,31 @@
+#include "cw_compat.hpp"
 #include <vector>
 #include <string>
 
-std::vector<int> vowelIndices(const std::string& word);
-#include "cw_compat.hpp"
+std::vector<int> vowelIndices(const std::string& word)
+{
+    std::vector<int> res;
+    char charArray[] = {'a', 'e', 'i', 'o', 'u', 'y'};
+
+    for (int i = 0; i < static_cast<int>(word.size()); ++i)
+    {
+        for (const auto& c : charArray)
+        {
+            if (tolower(word[i]) == c)
+            {
+                res.push_back(i + 1);
+            }
+        }
+    }
+
+    return res;
+}
 
 Describe(vowelIndicesTest)
 {
     It(SimpleTests)
     {
-        std::vector<int> res { 2, 4 };
+        const std::vector res { 2, 4 };
         Assert::That(vowelIndices("super"), Equals(res));
     }
 };
