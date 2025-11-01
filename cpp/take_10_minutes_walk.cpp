b9a06eaf5293c7801aa02a3c49dc5a7cbaaee84a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-01 17:43:49 +0800

    cpp/take_10_minutes_walk.cpp

--- cpp/take_10_minutes_walk.cpp
+++ cpp/take_10_minutes_walk.cpp
@@ -1,8 +1,45 @@
 #include "cw_compat.hpp"
 #include<vector>
 
-bool isValidWalk(std::vector<char> walk) {
-    //your code here
+using namespace std;
+
+bool isValidWalk(const std::vector<char>& walk)
+{
+    std::pair coordinates = {0, 0};
+    int counter = 0;
+
+    for (const auto& c : walk)
+    {
+        ++counter;
+
+        if (c == 'n')
+        {
+            coordinates.second += 1;
+
+            continue;
+        }
+
+        if (c == 's')
+        {
+            coordinates.second -= 1;
+
+            continue;
+        }
+
+        if (c == 'w')
+        {
+            coordinates.first -= 1;
+
+            continue;
+        }
+
+        if (c == 'e')
+        {
+            coordinates.first += 1;
+        }
+    }
+
+    return counter == 10 && coordinates.first == 0 && coordinates.second == 0;
 }
 
 using V = std::vector<char>;
@@ -12,14 +49,15 @@ Describe(IsValidWalk)
     It(BasicTests)
     {
         Assert::That(isValidWalk(V{'n'}), Equals(false));
-        Assert::That(isValidWalk(V{'n','s','n','s','n','s','n','s','n','s'}), Equals(true));
-        Assert::That(isValidWalk(V{'n','s'}), Equals(false));
-        Assert::That(isValidWalk(V{'n','s','n','s','n','s','n','s','n','s','n','s'}), Equals(false));
-        Assert::That(isValidWalk(V{'e','w','e','w','n','s','n','s','e','w'}), Equals(true));
-        Assert::That(isValidWalk(V{'n','s','e','w','n','s','e','w','n','s','e','w','n','s','e','w'}), Equals(false));
-        Assert::That(isValidWalk(V{'n','s','e','w','n','s','e','w','n','s'}), Equals(true));
-        Assert::That(isValidWalk(V{'s','e','w','n','n','s','e','w','n','s'}), Equals(true));
-        Assert::That(isValidWalk(V{'n','s','n','s','n','s','n','s','n','n'}), Equals(false));
-        Assert::That(isValidWalk(V{'e','e','e','w','n','s','n','s','e','w'}), Equals(false));
+        Assert::That(isValidWalk(V{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}), Equals(true));
+        Assert::That(isValidWalk(V{'n', 's'}), Equals(false));
+        Assert::That(isValidWalk(V{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}), Equals(false));
+        Assert::That(isValidWalk(V{'e', 'w', 'e', 'w', 'n', 's', 'n', 's', 'e', 'w'}), Equals(true));
+        Assert::That(isValidWalk(V{'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's', 'e', 'w'}),
+                     Equals(false));
+        Assert::That(isValidWalk(V{'n', 's', 'e', 'w', 'n', 's', 'e', 'w', 'n', 's'}), Equals(true));
+        Assert::That(isValidWalk(V{'s', 'e', 'w', 'n', 'n', 's', 'e', 'w', 'n', 's'}), Equals(true));
+        Assert::That(isValidWalk(V{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 'n'}), Equals(false));
+        Assert::That(isValidWalk(V{'e', 'e', 'e', 'w', 'n', 's', 'n', 's', 'e', 'w'}), Equals(false));
     }
 };
