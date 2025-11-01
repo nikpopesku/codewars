67f404722315368a67d91de90dd55995b7a7ffb1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-01 17:30:09 +0800

    cpp/who_likes_it.cpp

--- cpp/who_likes_it.cpp
+++ cpp/who_likes_it.cpp
@@ -2,16 +2,34 @@
 #include <vector>
 #include "cw_compat.hpp"
 
-std::string likes(const std::vector<std::string> &names)
+using namespace std;
+
+
+string likes(const vector<string>& names)
 {
-    return ""; // Do your magic!
+    if (names.empty())
+    {
+        return "no one likes this";
+    }
+
+    if (names.size() == 1)
+    {
+        return names[0] + " likes this";
+    }
+
+    if (names.size() == 2)
+    {
+        return names[0] + " and " + names[1] + " like this";
+    }
+
+    if (names.size() == 3)
+    {
+        return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+    }
+
+    return names[0] + ", " + names[1] + " and " + to_string(names.size() - 2) + " others like this";
 }
 
-
-#include <string>
-#include <vector>
-
-std::string likes(const std::vector<std::string> &names);
 
 Describe(sample_tests)
 {
