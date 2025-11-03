1c8f1dfec1f643dd67e9ce9d93cd3ff69f69d7e1
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-03 14:02:42 +0800

    cpp/6kyu/build_tower.cpp

--- cpp/6kyu/build_tower.cpp
+++ cpp/6kyu/build_tower.cpp
@@ -2,8 +2,10 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> towerBuilder(unsigned nFloors) {
-    return std::vector<std::string>();
+using namespace std;
+
+vector<string> towerBuilder(unsigned nFloors) {
+    return vector<string>();
 }
 
 #include <string>
@@ -13,27 +15,27 @@ Describe(Tests)
 {
     It(ExampleTest1)
     {
-        std::vector<std::string> expected = { "*" };
+        vector<string> expected = { "*" };
 
-        std::vector<std::string> actual = towerBuilder(1);
+        vector<string> actual = towerBuilder(1);
 
         Assert::That(actual, Is().EqualTo(expected));
     }
 
     It(ExampleTest2)
     {
-        std::vector<std::string> expected = { " * ", "***" };
+        vector<string> expected = { " * ", "***" };
 
-        std::vector<std::string> actual = towerBuilder(2);
+        vector<string> actual = towerBuilder(2);
 
         Assert::That(actual, Is().EqualTo(expected));
     }
 
     It(ExampleTest3)
     {
-        std::vector<std::string> expected = { "  *  ", " *** ", "*****" };
+        vector<string> expected = { "  *  ", " *** ", "*****" };
 
-        std::vector<std::string> actual = towerBuilder(3);
+        vector<string> actual = towerBuilder(3);
 
         Assert::That(actual, Is().EqualTo(expected));
     }
