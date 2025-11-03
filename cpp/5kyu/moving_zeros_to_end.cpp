33d38adb9b82df3288d4f0daeea4da0f197fce83
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-03 15:09:52 +0800

    cpp/5kyu/moving_zeros_to_end.cpp

--- cpp/5kyu/moving_zeros_to_end.cpp
+++ cpp/5kyu/moving_zeros_to_end.cpp
@@ -5,23 +5,20 @@ using namespace std;
 
 vector<int> move_zeroes(const vector<int>& input) {
     // your code here
-    vector<int> response(1);
-    response[0] = input[0];
-
-    return response;
+    return {};
 }
 
 Describe(Fixed_tests) {
     It(Mixed_data) {
-        Assert::That(move_zeroes(vector<int>{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}),
-                     Equals(vector<int>{1, 2, 1, 1, 3, 1, 0, 0, 0, 0}));
+        Assert::That(move_zeroes(vector{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}),
+                     Equals((vector{1, 2, 1, 1, 3, 1, 0, 0, 0, 0})));
     }
     It(All_zeroes) {
-        Assert::That(move_zeroes(vector<int>{0, 0, 0, 0, 0, 0}),
-                     Equals(vector<int>{0, 0, 0, 0, 0, 0}));
+        Assert::That(move_zeroes(vector{0, 0, 0, 0, 0, 0}),
+                     Equals((vector{0, 0, 0, 0, 0, 0})));
     }
     It(No_zeroes) {
-        Assert::That(move_zeroes(vector<int>{1, 2, 5, 1, 5, 1, 5, 3, 2, 1}),
-                     Equals(vector<int>{1, 2, 5, 1, 5, 1, 5, 3, 2, 1}));
+        Assert::That(move_zeroes(vector{1, 2, 5, 1, 5, 1, 5, 3, 2, 1}),
+                     Equals((vector{1, 2, 5, 1, 5, 1, 5, 3, 2, 1})));
     }
 };
