b0b40571684b67d5e382765ab9e9ee6b9893e25b
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-03 14:58:59 +0800

    cpp/5kyu/moving_zeros_to_end.cpp

--- cpp/5kyu/moving_zeros_to_end.cpp
+++ cpp/5kyu/moving_zeros_to_end.cpp
@@ -9,13 +9,13 @@ Describe(Fixed_tests) {
     It(Mixed_data) {
         Assert::That(move_zeroes(std::vector<int>{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}),
                      Equals(std::vector<int>{1, 2, 1, 1, 3, 1, 0, 0, 0, 0}));
-    };
+    }
     It(All_zeroes) {
         Assert::That(move_zeroes(std::vector<int>{0, 0, 0, 0, 0, 0}),
                      Equals(std::vector<int>{0, 0, 0, 0, 0, 0}));
-    };
+    }
     It(No_zeroes) {
         Assert::That(move_zeroes(std::vector<int>{1, 2, 5, 1, 5, 1, 5, 3, 2, 1}),
                      Equals(std::vector<int>{1, 2, 5, 1, 5, 1, 5, 3, 2, 1}));
-    };
-}
+    }
+};
