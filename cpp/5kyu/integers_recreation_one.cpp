d5494b68a2a9e05a6edd125dfa9905b0388da28f
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-05 15:35:32 +0800

    cpp/5kyu/integers_recreation_one.cpp

--- cpp/5kyu/integers_recreation_one.cpp
+++ cpp/5kyu/integers_recreation_one.cpp
@@ -2,34 +2,27 @@
 #include <utility>
 #include <vector>
 
-class SumSquaredDivisors
-{
+class SumSquaredDivisors {
 public:
-    static std::vector<std::pair<long long, long long>> listSquared(long long m, long long n);
+    static std::vector<std::pair<long long, long long> > listSquared(long long m, long long n) {
+    }
 };
 
-using Result = std::vector<std::pair<long long, long long>>;
-void testequal(const Result& ans, const Result& sol)
-{
+using Result = std::vector<std::pair<long long, long long> >;
+
+void testequal(const Result &ans, const Result &sol) {
     Assert::That(ans, Equals(sol));
 }
-void dotest(long long m, long long n, const Result& expected)
-{
+
+void dotest(long long m, long long n, const Result &expected) {
     testequal(SumSquaredDivisors::listSquared(m, n), expected);
 }
 
-Describe(listSquared_Tests)
-{
-    It(Fixed_Tests)
-    {
+Describe(listSquared_Tests) {
+    It(Fixed_Tests) {
         dotest(1, 250, {{1, 1}, {42, 2500}, {246, 84100}});
         dotest(42, 250, {{42, 2500}, {246, 84100}});
         dotest(250, 500, {{287, 84100}});
         dotest(300, 600, {});
     }
 };
-
-
-
-
-
