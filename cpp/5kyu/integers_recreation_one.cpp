282c4cd42a7260fa20f669813d1ed1c0dfbcce1f
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-05 15:57:48 +0800

    cpp/5kyu/integers_recreation_one.cpp

--- cpp/5kyu/integers_recreation_one.cpp
+++ cpp/5kyu/integers_recreation_one.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class SumSquaredDivisors {
 public:
     static vector<pair<long long, long long> > listSquared(const long long m, const long long n) {
-        vector<pair<long long, long long>> response;
+        vector<pair<long long, long long> > response;
 
         for (long long i = m; i < n; ++i) {
             vector<long long> divisors = {1};
@@ -21,7 +21,9 @@ public:
             while (divisor * divisor <= i) {
                 if (i % divisor == 0) {
                     divisors.push_back(divisor);
-                    divisors.push_back(i / divisor);
+                    if (i / divisor != divisor) {
+                        divisors.push_back(i / divisor);
+                    }
                 }
                 ++divisor;
             }
@@ -32,7 +34,7 @@ public:
                 value += d * d;
             }
 
-            if (double sr = sqrt(value); ceil(sr) == floor(sr)) {
+            if (const double sr = sqrt(value); ceil(sr) == floor(sr)) {
                 response.push_back({i, value});
             }
         }
@@ -53,9 +55,9 @@ void dotest(long long m, long long n, const Result &expected) {
 
 Describe(listSquared_Tests) {
     It(Fixed_Tests) {
-        // dotest(1, 250, {{1, 1}, {42, 2500}, {246, 84100}});
-        // dotest(42, 250, {{42, 2500}, {246, 84100}});
-        // dotest(250, 500, {{287, 84100}});
+        dotest(1, 250, {{1, 1}, {42, 2500}, {246, 84100}});
+        dotest(42, 250, {{42, 2500}, {246, 84100}});
+        dotest(250, 500, {{287, 84100}});
         dotest(300, 600, {});
     }
 };
