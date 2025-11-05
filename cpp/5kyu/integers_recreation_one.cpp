f695bad3c6ce84165b6d24808f53e5e30c49e8a0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-05 15:32:40 +0800

    cpp/5kyu/integers_recreation_one.cpp

---
+++ cpp/5kyu/integers_recreation_one.cpp
@@ -0,0 +1,35 @@
+#include "../cw_compat.hpp"
+#include <utility>
+#include <vector>
+
+class SumSquaredDivisors
+{
+public:
+    static std::vector<std::pair<long long, long long>> listSquared(long long m, long long n);
+};
+
+using Result = std::vector<std::pair<long long, long long>>;
+void testequal(const Result& ans, const Result& sol)
+{
+    Assert::That(ans, Equals(sol));
+}
+void dotest(long long m, long long n, const Result& expected)
+{
+    testequal(SumSquaredDivisors::listSquared(m, n), expected);
+}
+
+Describe(listSquared_Tests)
+{
+    It(Fixed_Tests)
+    {
+        dotest(1, 250, {{1, 1}, {42, 2500}, {246, 84100}});
+        dotest(42, 250, {{42, 2500}, {246, 84100}});
+        dotest(250, 500, {{287, 84100}});
+        dotest(300, 600, {});
+    }
+};
+
+
+
+
+
