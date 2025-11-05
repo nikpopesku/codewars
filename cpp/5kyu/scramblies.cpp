72e00d14037de4fb30a66a8966679ceabf8a8e07
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-05 15:29:12 +0800

    cpp/5kyu/scramblies.cpp

--- cpp/5kyu/scramblies.cpp
+++ cpp/5kyu/scramblies.cpp
@@ -1,45 +1,80 @@
 #include "../cw_compat.hpp"
-#include <string>
-#include <unordered_map>
+#include <array>
+#include <map>
 
 using namespace std;
 
-bool scramble(const std::string &s1, const std::string &s2) {
-    unordered_map<char, int> mp;
+int score(const std::array<unsigned, 5> &dice) {
+    map<unsigned, int> mp;
 
-    for (const char &c: s2) {
-        ++mp[c];
+    for (const unsigned &d: dice) {
+        ++mp[d];
     }
 
-    for (const char &c: s1) {
-        if (mp.count(c)) {
-            --mp[c];
+    int response = 0;
 
-            if (mp[c] == 0) {
-                mp.erase(c);
+    while (true) {
+        const int response_before = response;
+
+        for (auto &[fst, snd]: mp) {
+            if (fst == 1 && snd >= 3) {
+                mp[fst] -= 3;
+                response += 1000;
+            }
+            if (fst == 6 && snd >= 3) {
+                mp[fst] -= 3;
+                response += 600;
+            }
+            if (fst == 5 && snd >= 3) {
+                mp[fst] -= 3;
+                response += 500;
+            }
+            if (fst == 4 && snd >= 3) {
+                mp[fst] -= 3;
+                response += 400;
+            }
+            if (fst == 3 && snd >= 3) {
+                mp[fst] -= 3;
+                response += 300;
+            }
+            if (fst == 2 && snd >= 3) {
+                mp[fst] -= 3;
+                response += 200;
+            }
+            if (fst == 1 && snd >= 1) {
+                --mp[fst];
+                response += 100;
+            }
+            if (fst == 5 && snd >= 1) {
+                --mp[fst];
+                response += 50;
             }
         }
 
-        if (mp.empty()) {
-            return true;
+        if (response == response_before) {
+            break;
         }
     }
 
-    return false;
+    return response;
 }
 
 
-Describe(Scramble) {
-    It(BasicTests) {
-        Assert::That(scramble("rkqodlw", "world"), Equals(true));
-        Assert::That(scramble("cedewaraaossoqqyt", "codewars"), Equals(true));
-        Assert::That(scramble("katas", "steak"), Equals(false));
-        Assert::That(scramble("scriptjavx", "javascript"), Equals(false));
-        Assert::That(scramble("scriptingjava", "javascript"), Equals(true));
-        Assert::That(scramble("scriptsjava", "javascript"), Equals(true));
-        Assert::That(scramble("javscripts", "javascript"), Equals(false));
-        Assert::That(scramble("aabbcamaomsccdd", "commas"), Equals(true));
-        Assert::That(scramble("commas", "commas"), Equals(true));
-        Assert::That(scramble("sammoc", "commas"), Equals(true));
+int score(const std::array<unsigned, 5> &dice);
+
+Describe(ScorerFunction) {
+    It(should_value_this_as_worthless) {
+        std::array<unsigned, 5> dice = {2, 3, 4, 6, 2};
+        Assert::That(score(dice), Equals(0));
+    }
+
+    It(should_value_this_triplet_correctly) {
+        std::array<unsigned, 5> dice = {4, 4, 4, 3, 3};
+        Assert::That(score(dice), Equals(400));
+    }
+
+    It(should_value_this_mixed_set_correctly) {
+        std::array<unsigned, 5> dice = {2, 4, 4, 5, 4};
+        Assert::That(score(dice), Equals(450));
     }
 };
