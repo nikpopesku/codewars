1c91f1d4e13cbdff5cccef0524b66d74c2df462a
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-05 15:51:21 +0800

    cpp/5kyu/integers_recreation_one.cpp

--- cpp/5kyu/integers_recreation_one.cpp
+++ cpp/5kyu/integers_recreation_one.cpp
@@ -2,13 +2,45 @@
 #include <utility>
 #include <vector>
 
+using namespace std;
+
 class SumSquaredDivisors {
 public:
-    static std::vector<std::pair<long long, long long> > listSquared(long long m, long long n) {
+    static vector<pair<long long, long long> > listSquared(const long long m, const long long n) {
+        vector<pair<long long, long long>> response;
+
+        for (long long i = m; i < n; ++i) {
+            vector<long long> divisors = {1};
+
+            if (i > 1) {
+                divisors.push_back(i);
+            }
+
+            long long divisor = 2;
+            while (divisor * divisor <= i) {
+                if (i % divisor == 0) {
+                    divisors.push_back(divisor);
+                    divisors.push_back(i / divisor);
+                }
+                ++divisor;
+            }
+
+            long long value = 0;
+
+            for (const auto &d: divisors) {
+                value += d * d;
+            }
+
+            if (double sr = sqrt(value); ceil(sr) == floor(sr)) {
+                response.push_back({i, value});
+            }
+        }
+
+        return response;
     }
 };
 
-using Result = std::vector<std::pair<long long, long long> >;
+using Result = vector<pair<long long, long long> >;
 
 void testequal(const Result &ans, const Result &sol) {
     Assert::That(ans, Equals(sol));
