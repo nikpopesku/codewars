2187013992f145d0b2fb91f0cb4e37c1ffbd2800
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-07 14:40:27 +0800

    cpp/5kyu/int32_to_ipv4.cpp

--- cpp/5kyu/int32_to_ipv4.cpp
+++ cpp/5kyu/int32_to_ipv4.cpp
@@ -4,16 +4,20 @@
 using namespace std;
 
 
-std::string uint32_to_ip(uint32_t ip)
-{
-    // your code here
-    return "";
+string uint32_to_ip(uint32_t ip) {
+    string response;
+
+    for (int i = 3; i >= 0; ++i) {
+        const uint32_t mx = 255 << (i * 8);
+        uint32_t val = ip & mx;
+        val = val >> (i * 8);
+        response += to_string(val) + ".";
+    }
+    return response;
 }
 
-Describe(test_cases)
-{
-    It(sample_tests)
-    {
+Describe(test_cases) {
+    It(sample_tests) {
         Assert::That(uint32_to_ip(2154959208), Equals("128.114.17.104"));
         Assert::That(uint32_to_ip(0), Equals("0.0.0.0"));
         Assert::That(uint32_to_ip(2149583361), Equals("128.32.10.1"));
