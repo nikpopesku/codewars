0b026f5e89b062e89b997c2601951228da4e8bb4
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-07 14:18:34 +0800

    cpp/5kyu/pick_peaks.cpp

--- cpp/5kyu/pick_peaks.cpp
+++ cpp/5kyu/pick_peaks.cpp
@@ -14,14 +14,14 @@ PeakData pick_peaks(const std::vector<int> &v) {
     int local_max_peak = 0;
 
     for (int i = 1; i < static_cast<int>(v.size()); ++i) {
-        if (v[i] > v[i-1]) {
+        if (v[i] > v[i - 1]) {
             local_max_left = true;
             local_max = i;
             local_max_peak = v[i];
 
             continue;
         }
-        if (v[i] < v[i-1] && local_max_left) {
+        if (v[i] < v[i - 1] && local_max_left) {
             pd.pos.push_back(local_max);
             pd.peaks.push_back(local_max_peak);
             local_max_left = false;
@@ -35,15 +35,15 @@ Describe(your_pick_peaks_function) {
     It(should_pass_the_sample_test_cases_provided_in_the_description) {
         PeakData actual, expected;
 
-        actual = pick_peaks(std::vector<int> {3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3});
+        actual = pick_peaks(std::vector<int>{3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3});
         expected = {.pos = {3, 7}, .peaks = {6, 3}};
         Assert::That(actual, Equals(expected));
 
-        actual = pick_peaks(std::vector<int> {1, 2, 2, 2, 1});
+        actual = pick_peaks(std::vector<int>{1, 2, 2, 2, 1});
         expected = {.pos = {1}, .peaks = {2}};
         Assert::That(actual, Equals(expected));
 
-        actual = pick_peaks(std::vector<int> {1, 2, 2, 2, 3});
+        actual = pick_peaks(std::vector<int>{1, 2, 2, 2, 3});
         expected = {.pos = {}, .peaks = {}};
         Assert::That(actual, Equals(expected));
     }
