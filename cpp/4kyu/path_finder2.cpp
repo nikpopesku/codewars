6c8ff1b26f4f12a6f04089411eb5c461ad527f52
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-12 16:51:11 +0800

    cpp/4kyu/path_finder2.cpp

--- cpp/4kyu/path_finder2.cpp
+++ cpp/4kyu/path_finder2.cpp
@@ -10,24 +10,49 @@ int path_finder(const string &maze) {
     string s;
     vector<string> mz;
     unordered_set<string> visited;
+    int N = mz.size();
 
     while (std::getline(ss, s, ',')) {
         mz.push_back(s);
     }
 
+    pair position = {0, 0};
+    stack<tuple<int, int, int> > st;
+    st.push({0, 0, 0});
 
-    for (int i = 0; i < static_cast<int>(mz.size()); ++i) {
-        for (int j = 0; j < static_cast<int>(mz[0].size()); ++j) {
-            if (mz[i][j] == 'W') {
-                continue;
-            }
+    vector<pair<int, int> > directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    int response = N * N;
 
-            s = to_string(i) + '_' + to_string(j);
-            if (visited.count(s)) {
-                continue;
+    while (!st.empty()) {
+        auto [i, j, count] = st.top();
+        st.pop();
+        if (mz[i][j] == 'W') {
+            continue;
+        }
+
+        if (i == N - 1 && j == N - 1) {
+            response = min(response, count);
+        }
+
+        for (auto &[fst, snd]: directions) {
+            pair nd = {position.first + fst, position.second + snd};
+
+            if (nd.first >= 0 && nd.first < static_cast<int>(mz.size())
+                && nd.second >= 0 && nd.second < static_cast<int>(mz.size()) && mz[nd.first][nd.second] != 'W' &&
+                visited.count(to_string(nd.first) + '_' + to_string(nd.second)) == 0) {
+                visited.insert(to_string(nd.first) + '_' + to_string(nd.second));
+                st.push({nd.first, nd.second, count + 1});
             }
         }
+
+        s = to_string(i) + '_' + to_string(j);
+        if (visited.count(s)) {
+            continue;
+        }
+
+        st.push({i, j});
     }
+
     return -1;
 }
 
