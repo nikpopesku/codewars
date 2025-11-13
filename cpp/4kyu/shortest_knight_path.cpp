2d849d480f0e6e2e3f9b9dbb990f2d6cfa010726
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-13 12:27:02 +0800

    cpp/4kyu/shortest_knight_path.cpp

--- cpp/4kyu/shortest_knight_path.cpp
+++ cpp/4kyu/shortest_knight_path.cpp
@@ -26,19 +26,18 @@ int get_row(const char a) {
     }
 }
 
-int knight(const string& start, const string &finish) {
+int knight(const string &start, const string &finish) {
     pair bg = {get_row(start[0]), start[1] - '0'};
     const pair end = {get_row(finish[0]), finish[1] - '0'};
 
     vector dp(8, vector(8, 64));
-    stack<tuple<int, int, int>> st;
+    stack<tuple<int, int, int> > st;
     st.emplace(bg.first, bg.second, 0);
+    vector<pair<int, int> > directions = {{1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
 
     while (!st.empty()) {
         auto [row, col, count] = st.top();
         st.pop();
-
-
     }
 
     return dp[end.first][end.second];
