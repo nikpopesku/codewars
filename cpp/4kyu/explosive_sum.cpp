101bea66569172d3144a97237c159dbbe38461cb
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-14 11:55:18 +0800

    UN-26835: Adjusted subscriber

--- cpp/4kyu/explosive_sum.cpp
+++ cpp/4kyu/explosive_sum.cpp
@@ -1,14 +1,28 @@
 #include "../cw_compat.hpp"
+
+using namespace std;
+
 using ull = unsigned long long;
 
-ull exp_sum(unsigned int n) {
-    return 0;
+ull exp_sum(const unsigned int n) {
+    if (n == 0) {
+        return 1;
+    }
+
+    vector<ull> dp(n + 1, 0);
+    dp[0] = 1;
+
+    for (unsigned int k = 1; k <= n; ++k) {
+        for (unsigned int i = k; i <= n; ++i) {
+            dp[i] += dp[i - k];
+        }
+    }
+
+    return dp[n];
 }
 
-Describe(Sample_Tests)
-{
-    It(Tests)
-    {
+Describe(Sample_Tests) {
+    It(Tests) {
         Assert::That(exp_sum(1), Equals(1));
         Assert::That(exp_sum(2), Equals(2));
         Assert::That(exp_sum(3), Equals(3));
