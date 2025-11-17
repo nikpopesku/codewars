cee155a8a18bb2023ed5102e661344aa6b6e4e7c
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-17 12:42:05 +0800

    cpp/3kyu/path_finder3.cpp

--- cpp/3kyu/path_finder3.cpp
+++ cpp/3kyu/path_finder3.cpp
@@ -50,21 +50,63 @@ Describe(example_tests)
 {
     It(should_work_for_basic_tests)
     {
-        string s1 =
+        // string s1 =
+        //
+        //     "000\n"
+        //     "000\n"
+        //     "000";
+        // Assert::That(path_finder(s1), Equals(0));
+        //
+        // string s2 =
+        //
+        //     "010\n"
+        //     "010\n"
+        //     "010";
+        // Assert::That(path_finder(s2), Equals(2));
 
-            "000\n"
-            "000\n"
-            "000";
-        Assert::That(path_finder(s1), Equals(0));
-
-        string s2 =
+        string s3 =
 
             "010\n"
-            "010\n"
+            "101\n"
             "010";
-        Assert::That(path_finder(s2), Equals(2));
+        Assert::That(path_finder(s3), Equals(4));
 
-        // string s3 =
-pp(s7), Equals(4));
+        // string s4 =
+        //
+        //     "0707\n"
+        //     "7070\n"
+        //     "0707\n"
+        //     "7070";
+        // Assert::That(path_finder(s4), Equals(42));
+        //
+        // string s5 =
+        //
+        //     "700000\n"
+        //     "077770\n"
+        //     "077770\n"
+        //     "077770\n"
+        //     "077770\n"
+        //     "000007";
+        // Assert::That(path_finder(s5), Equals(14));
+        //
+        // string s6 =
+        //
+        //     "777000\n"
+        //     "007000\n"
+        //     "007000\n"
+        //     "007000\n"
+        //     "007000\n"
+        //     "007777";
+        // Assert::That(path_finder(s6), Equals(0));
+        //
+        // string s7 =
+        //
+        //     "000000\n"
+        //     "000000\n"
+        //     "000000\n"
+        //     "000010\n"
+        //     "000109\n"
+        //     "001010";
+        // Assert::That(path_finder(s7), Equals(4));
     }
 };
