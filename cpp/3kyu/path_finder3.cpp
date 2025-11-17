44d99486a1fd8e0b3f5bc3f7b342cc98c06b7b23
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-17 12:35:58 +0800

    cpp/3kyu/path_finder3.cpp

--- cpp/3kyu/path_finder3.cpp
+++ cpp/3kyu/path_finder3.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 int path_finder(const string& maze)
 {
-    const int N = static_cast<int>(maze.size());
-
     stringstream ss(maze);
     string line;
     vector<string> lines;
@@ -15,8 +13,9 @@ int path_finder(const string& maze)
         lines.push_back(line);
     }
 
+    const int N = static_cast<int>(lines.size());
+
     vector dp(N, vector(N, INT_MAX));
-    dp[0][0] = 0;
 
 
     const vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
@@ -38,7 +37,7 @@ int path_finder(const string& maze)
                 pair nd = {row + fst, col + snd};
                 if (nd.first >= 0 && nd.first < N && nd.second >= 0 && nd.second < N)
                 {
-                    st.emplace(nd.first, nd.second, count + abs(lines[nd.first][nd.second] - dp[row][col]));
+                    st.emplace(nd.first, nd.second, count + abs((lines[nd.first][nd.second] - '0') - dp[row][col]));
                 }
             }
         }
@@ -65,49 +64,7 @@ Describe(example_tests)
             "010";
         Assert::That(path_finder(s2), Equals(2));
 
-        string s3 =
-
-            "010\n"
-            "101\n"
-            "010";
-        Assert::That(path_finder(s3), Equals(4));
-
-        string s4 =
-
-            "0707\n"
-            "7070\n"
-            "0707\n"
-            "7070";
-        Assert::That(path_finder(s4), Equals(42));
-
-        string s5 =
-
-            "700000\n"
-            "077770\n"
-            "077770\n"
-            "077770\n"
-            "077770\n"
-            "000007";
-        Assert::That(path_finder(s5), Equals(14));
-
-        string s6 =
-
-            "777000\n"
-            "007000\n"
-            "007000\n"
-            "007000\n"
-            "007000\n"
-            "007777";
-        Assert::That(path_finder(s6), Equals(0));
-
-        string s7 =
-
-            "000000\n"
-            "000000\n"
-            "000000\n"
-            "000010\n"
-            "000109\n"
-            "001010";
-        Assert::That(path_finder(s7), Equals(4));
+        // string s3 =
+pp(s7), Equals(4));
     }
 };
