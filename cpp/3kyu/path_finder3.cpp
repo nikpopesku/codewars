3d5d856631339a12573077e72cd23a3780138d25
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-18 12:50:57 +0800

    cpp/3kyu/path_finder3.cpp

--- cpp/3kyu/path_finder3.cpp
+++ cpp/3kyu/path_finder3.cpp
@@ -4,122 +4,92 @@
 
 using namespace std;
 
-int path_finder(const string& maze)
-{
+int path_finder(const string &maze) {
     stringstream ss(maze);
     string line;
     vector<string> lines;
-    while (getline(ss, line, '\n'))
-    {
+
+    while (getline(ss, line, '\n')) {
         lines.push_back(line);
     }
 
     const int N = static_cast<int>(lines.size());
 
     vector dp(N, vector(N, INT_MAX));
-    dp[0][0] = 0;
 
-    const vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
-
-    // Priority queue for Dijkstra's algorithm: {cost, row, col}
-    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+    priority_queue<tuple<int, int, int>, vector<tuple<int, int, int> >, greater<> > pq;
     pq.emplace(0, 0, 0);
 
-    while (!pq.empty())
-    {
+    while (!pq.empty()) {
         auto [cost, row, col] = pq.top();
         pq.pop();
 
         if (cost > dp[row][col]) {
-            continue; // Skip if we already found a better path
-        }
-
-        if (row == N - 1 && col == N - 1) {
-            return cost; // Early exit when reaching target
-        }
-
-        for (const auto& [dr, dc] : directions)
-        {
-            int new_row = row + dr;
-            int new_col = col + dc;
-
-            if (new_row >= 0 && new_row < N && new_col >= 0 && new_col < N)
-            {
-                int new_cost = cost + abs((lines[new_row][new_col] - '0') - (lines[row][col] - '0'));
-
-                if (new_cost < dp[new_row][new_col]) {
-                    dp[new_row][new_col] = new_cost;
-                    pq.emplace(new_cost, new_row, new_col);
-                }
-            }
+            continue;
         }
     }
-
-    return dp[N - 1][N - 1];
 }
 
-Describe(example_tests)
-{
-    It(should_work_for_basic_tests)
-    {
+Describe(example_tests) {
+    It(should_work_for_basic_tests) {
         string s1 =
 
-            "000\n"
-            "000\n"
-            "000";
+                "000\n"
+                "000\n"
+                "000";
         Assert::That(path_finder(s1), Equals(0));
 
         string s2 =
 
-            "010\n"
-            "010\n"
-            "010";
+                "010\n"
+                "010\n"
+                "010";
         Assert::That(path_finder(s2), Equals(2));
 
         string s3 =
 
-            "010\n"
-            "101\n"
-            "010";
+                "010\n"
+                "101\n"
+                "010";
         Assert::That(path_finder(s3), Equals(4));
 
         string s4 =
 
-            "0707\n"
-            "7070\n"
-            "0707\n"
-            "7070";
+                "0707\n"
+                "7070\n"
+                "0707\n"
+                "7070";
         Assert::That(path_finder(s4), Equals(42));
 
         string s5 =
 
-            "700000\n"
-            "077770\n"
-            "077770\n"
-            "077770\n"
-            "077770\n"
-            "000007";
+                "700000\n"
+                "077770\n"
+                "077770\n"
+                "077770\n"
+                "077770\n"
+                "000007";
         Assert::That(path_finder(s5), Equals(14));
 
         string s6 =
 
-            "777000\n"
-            "007000\n"
-            "007000\n"
-            "007000\n"
-            "007000\n"
-            "007777";
+                "777000\n"
+                "007000\n"
+                "007000\n"
+                "007000\n"
+                "007000\n"
+                "007777";
         Assert::That(path_finder(s6), Equals(0));
 
         string s7 =
 
 
-            "000000\n"
-            "000000\n"
-            "000000\n"
-            "000010\n"
-            "000109\n"
-            "001010";
+                "000000\n"
+                "000000\n"
+                "000000\n"
+                "000010\n"
+                "000109\n"
+                "001010";
         Assert::That(path_finder(s7), Equals(4));
     }
 };
