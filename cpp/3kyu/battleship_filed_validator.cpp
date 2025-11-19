a7ea128b459c84a0dba520e395be6153d3a99176
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-19 11:57:21 +0800

    cpp/3kyu/battleship_filed_validator.cpp

--- cpp/3kyu/battleship_filed_validator.cpp
+++ cpp/3kyu/battleship_filed_validator.cpp
@@ -12,9 +12,17 @@ struct PairHash {
 bool validate_battlefield(vector<vector<int> > field) {
     unordered_set<pair<int, int>, PairHash> s;
 
+
     for (int row = 0; row < 10; ++row) {
         for (int col = 0; col < 10; ++col) {
             if (s.count({row, col}) == 0) {
+                s.insert({row, col});
+
+                if (field[row][col] == 0) {
+                    continue;
+                }
+
+
             }
         }
     }
