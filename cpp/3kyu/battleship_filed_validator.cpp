454e73df7d7ed8b132c8345241713e107e90daed
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-19 11:48:44 +0800

    cpp/3kyu/battleship_filed_validator.cpp

--- cpp/3kyu/battleship_filed_validator.cpp
+++ cpp/3kyu/battleship_filed_validator.cpp
@@ -3,25 +3,23 @@
 
 using namespace std;
 
-bool validate_battlefield(std::vector< std::vector<int> > field) {
-    // Write your magic here ;)
-    return true;
+bool validate_battlefield(vector<vector<int> > field) {
 }
 
 
 Describe(your_validate_battlefield_function) {
     It(should_work_for_a_sample_test_case) {
-        Assert::That(validate_battlefield(std::vector< std::vector<int> > {
-          std::vector<int> {1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
-          std::vector<int> {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
-          std::vector<int> {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
-          std::vector<int> {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-          std::vector<int> {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-          std::vector<int> {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-          std::vector<int> {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
-          std::vector<int> {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
-          std::vector<int> {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
-          std::vector<int> {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-        }), Equals(true));
+        Assert::That(validate_battlefield(vector{
+                         vector{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                         vector{1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                         vector{1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                         vector{1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                         vector{0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                         vector{0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                         vector{0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                         vector{0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                         vector{0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                         vector{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+                     }), Equals(true));
     }
 };
