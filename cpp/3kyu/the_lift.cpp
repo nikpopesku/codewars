2b37370554cc3dbe5739b2c9919e9d2be7540db0
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-21 11:42:43 +0800

    cpp/3kyu/the_lift.cpp

--- cpp/3kyu/the_lift.cpp
+++ cpp/3kyu/the_lift.cpp
@@ -3,29 +3,28 @@
 
 using namespace std;
 
-vector<int> the_lift(const vector<vector<int>> &queues, int capacity) {
+vector<int> the_lift(const vector<vector<int> > &queues, int capacity) {
     return {};
 }
 
-Describe(Sample_Tests)
-{
-    It(Tests)
-    {
-        vector<vector<int>> queues; vector<int> result;
+Describe(Sample_Tests) {
+    It(Tests) {
+        vector<vector<int> > queues;
+        vector<int> result;
 
-        queues = { {}, {}, {5,5,5}, {}, {}, {}, {} };
+        queues = {{}, {}, {5, 5, 5}, {}, {}, {}, {}};
         result = {0, 2, 5, 0};
         Assert::That(the_lift(queues, 5), Equals(result));
 
-        queues = { {}, {}, {1,1}, {}, {}, {}, {} };
+        queues = {{}, {}, {1, 1}, {}, {}, {}, {}};
         result = {0, 2, 1, 0};
         Assert::That(the_lift(queues, 5), Equals(result));
 
-        queues = { {}, {3}, {4}, {}, {5}, {}, {} };
+        queues = {{}, {3}, {4}, {}, {5}, {}, {}};
         result = {0, 1, 2, 3, 4, 5, 0};
         Assert::That(the_lift(queues, 5), Equals(result));
 
-        queues = { {}, {0}, {}, {}, {2}, {3}, {} };
+        queues = {{}, {0}, {}, {}, {2}, {3}, {}};
         result = {0, 5, 4, 3, 2, 1, 0};
         Assert::That(the_lift(queues, 5), Equals(result));
     }
