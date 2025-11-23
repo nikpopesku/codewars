8b62504a4b5068672c62b5cbcb3ba7f68a422379
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-23 13:48:58 +0800

    cpp/3kyu/the_lift.cpp

--- cpp/3kyu/the_lift.cpp
+++ cpp/3kyu/the_lift.cpp
@@ -131,7 +131,7 @@ vector<int> the_lift(vector<vector<int>>& queues, const int capacity)
                 }
             }
 
-            if (add_floor_to_response && response[response.size() - 1] != floor)
+            if (add_floor_to_response && (response.empty() || response[response.size() - 1] != floor))
             {
                 response.push_back(floor);
             }
@@ -168,45 +168,45 @@ Describe(Sample_Tests)
         vector<vector<int>> queues;
         vector<int> result;
 
-        // queues = {{}, {}, {5, 5, 5}, {}, {}, {}, {}};
-        // result = {0, 2, 5, 0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
-        //
-        // queues = {{}, {}, {1, 1}, {}, {}, {}, {}};
-        // result = {0, 2, 1, 0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
-        //
-        // queues = {{}, {3}, {4}, {}, {5}, {}, {}};
-        // result = {0, 1, 2, 3, 4, 5, 0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
-        //
-        // queues = {{}, {0}, {}, {}, {2}, {3}, {}};
-        // result = {0, 5, 4, 3, 2, 1, 0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
+        queues = {{}, {}, {5, 5, 5}, {}, {}, {}, {}};
+        result = {0, 2, 5, 0};
+        Assert::That(the_lift(queues, 5), Equals(result));
+
+        queues = {{}, {}, {1, 1}, {}, {}, {}, {}};
+        result = {0, 2, 1, 0};
+        Assert::That(the_lift(queues, 5), Equals(result));
+
+        queues = {{}, {3}, {4}, {}, {5}, {}, {}};
+        result = {0, 1, 2, 3, 4, 5, 0};
+        Assert::That(the_lift(queues, 5), Equals(result));
+
+        queues = {{}, {0}, {}, {}, {2}, {3}, {}};
+        result = {0, 5, 4, 3, 2, 1, 0};
+        Assert::That(the_lift(queues, 5), Equals(result));
 
         //Up_and_down
         queues = {{3}, {2}, {0}, {2}, {}, {}, {5}};
         result = {0, 1, 2, 3, 6, 5, 3, 2, 0};
         Assert::That(the_lift(queues, 5), Equals(result));
 
-        // //Yoyo
-        // queues = {{}, {}, {4, 4, 4, 4}, {}, {2, 2, 2, 2}, {}, {}};
-        // result = {0, 2, 4, 2, 4, 2, 0};
-        // Assert::That(the_lift(queues, 2), Equals(result));
-        //
-        // //Lift_full_up
-        // queues = {{3,3,3,3,3,3}, {}, {}, {}, {}, {}, {} };
-        // result = {0, 3, 0, 3, 0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
+        //Yoyo
+        queues = {{}, {}, {4, 4, 4, 4}, {}, {2, 2, 2, 2}, {}, {}};
+        result = {0, 2, 4, 2, 4, 2, 0};
+        Assert::That(the_lift(queues, 2), Equals(result));
 
-        // //Empty_building
-        // queues = { {}, {}, {}, {}, {}, {}, {} };
-        // result = {0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
+        //Lift_full_up
+        queues = {{3,3,3,3,3,3}, {}, {}, {}, {}, {}, {} };
+        result = {0, 3, 0, 3, 0};
+        Assert::That(the_lift(queues, 5), Equals(result));
 
-        // //Highlander
-        // queues = { { {}, {2}, {3,3,3}, {1}, {}, {}, {} } };
-        // result = {0};
-        // Assert::That(the_lift(queues, 1), Equals(result));
+        //Empty_building
+        queues = { {}, {}, {}, {}, {}, {}, {} };
+        result = {0};
+        Assert::That(the_lift(queues, 5), Equals(result));
+
+        //Highlander
+        queues = { { {}, {2}, {3,3,3}, {1}, {}, {}, {} } };
+        result = {0};
+        Assert::That(the_lift(queues, 1), Equals(result));
     }
 };
