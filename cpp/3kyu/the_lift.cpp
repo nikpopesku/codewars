84b0f01cdca64b7f0d57246aec0e4c06f27f68a3
npopesku <nikolay.popesku@unbiased.co.uk>
2025-11-23 12:51:33 +0800

    cpp/3kyu/the_lift.cpp

--- cpp/3kyu/the_lift.cpp
+++ cpp/3kyu/the_lift.cpp
@@ -162,10 +162,10 @@ Describe(Sample_Tests)
         vector<vector<int>> queues;
         vector<int> result;
 
-        queues = {{}, {}, {5, 5, 5}, {}, {}, {}, {}};
-        result = {0, 2, 5, 0};
-        Assert::That(the_lift(queues, 5), Equals(result));
-
+        // queues = {{}, {}, {5, 5, 5}, {}, {}, {}, {}};
+        // result = {0, 2, 5, 0};
+        // Assert::That(the_lift(queues, 5), Equals(result));
+        //
         // queues = {{}, {}, {1, 1}, {}, {}, {}, {}};
         // result = {0, 2, 1, 0};
         // Assert::That(the_lift(queues, 5), Equals(result));
@@ -173,9 +173,9 @@ Describe(Sample_Tests)
         // queues = {{}, {3}, {4}, {}, {5}, {}, {}};
         // result = {0, 1, 2, 3, 4, 5, 0};
         // Assert::That(the_lift(queues, 5), Equals(result));
-        //
-        // queues = {{}, {0}, {}, {}, {2}, {3}, {}};
-        // result = {0, 5, 4, 3, 2, 1, 0};
-        // Assert::That(the_lift(queues, 5), Equals(result));
+
+        queues = {{}, {0}, {}, {}, {2}, {3}, {}};
+        result = {0, 5, 4, 3, 2, 1, 0};
+        Assert::That(the_lift(queues, 5), Equals(result));
     }
 };
